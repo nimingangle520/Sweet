833591274a963697105b47a784f6d09272675721
liming <nimingangle@163.com>
2022-06-28 19:22:33 +0800

    头部图片滚动布局

--- app/src/main/java/com/example/sweet/test/HeadScrollActivity.kt
+++ app/src/main/java/com/example/sweet/test/HeadScrollActivity.kt
@@ -1,14 +1,9 @@
 package com.example.sweet.test
 
-import android.view.Gravity
-import android.view.View
-import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 import com.drake.engine.base.EngineActivity
 import com.drake.statusbar.immersive
 import com.example.sweet.R
 import com.example.sweet.databinding.ActivityWeixinHeadBinding
-import kotlinx.android.synthetic.main.activity_head_scroll.*
 
 
 /**
